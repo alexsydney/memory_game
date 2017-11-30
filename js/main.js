3ffddc65c574e6a745118902af06bb8932fdd418
Alex Pham <alex.pham2010@gmail.com>
2017-11-30 19:08:55 +1100

    create object and use this

--- js/main.js
+++ js/main.js
@@ -38,26 +38,63 @@ console.log("User flipped " + cardTwo); */
  else
  	alert("Sorry, try again"); */
 
-
-  var flipCard = function(cardId){
-  	  //use console to access card 
-      console.log("User flipped" + cards[cardId]);
-      // Use the push method to add the card.
-      cardsInPlay.push(cards[cardId]);
-
-      cardsInPlay;
-
-      console.log("User flipped " + cards[cardId]);
-
-      checkForMatch();
-
-  }
-
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	console.log("You found a match!");
 	} else {
 	console.log("Sorry, try again.");
 	}
+};
+
+var myArray = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+},
+{
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
 }
+];
+
+
+
+  var flipCard = function(cardId){
+  	  //use console to access card 
+     // console.log("User flipped" + cards[cardId]);
+      // Use the push method to add the card.
+      //cardsInPlay.push(cards[cardId]);
+
+     // cardsInPlay;
+
+     //console.log("User flipped " + cards[cardId]);
+
+     // checkForMatch();
+
+     console.log("User flipped " + this.rank);
+
+     cardsInPlay.push(this.rank);
+
+     console.log("User flipped " + this.rank);
+
+
+  };
+
+
+
+
+
+
 
