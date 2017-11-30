1c3db0aef8d08eb1367ab6f15828bc9e865f5517
Alex Pham <alex.pham2010@gmail.com>
2017-11-30 17:10:11 +1100

    create game logic

--- js/main.js
+++ js/main.js
@@ -14,28 +14,50 @@ var cards = ["queen", "queen", "king","king"];
 var cardsInPlay = [];
 
 //cardOne store the fitst item in the cards
-var cardOne = cards[0];
+/*var cardOne = cards[0];
 
 cardsInPlay.push(cardOne);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped test " + cardsInPlay);
+console.log("User flipped " + cardTwo); */
 
 //cardTwo stored third item in the cards
-var cardTwo = cards[2];
+//var cardTwo = cards[2];
 
 //add cardTwo to end cardsInPlay
 
-cardsInPlay.push(cardTwo);
+//cardsInPlay.push(cardTwo);
 
 //display the card
-console.log("User flipped king " + cardsInPlay);
+// console.log("User flipped king " + cardsInPlay);
 
 
-if (cardsInPlay.length === 2)
-  cardsInPlay;
+/*  cardsInPlay;
  if(cardsInPlay[0] === cardsInPlay[1])
  	alert("You found a match");
  else
- 	alert("Sorry, try again");
-  
+ 	alert("Sorry, try again"); */
+
+
+  var flipCard = function(cardId){
+  	  //use console to access card 
+      console.log("User flipped" + cards[cardId]);
+      // Use the push method to add the card.
+      cardsInPlay.push(cards[cardId]);
+
+      cardsInPlay;
+
+      console.log("User flipped " + cards[cardId]);
+
+      checkForMatch();
+
+  }
+
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	} else {
+	console.log("Sorry, try again.");
+	}
+}
+
